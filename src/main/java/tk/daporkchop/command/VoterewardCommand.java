c054f9deeba2eebde1a514dc2ba0fb6bd9a9a2aa
DaMatrix <joey.rabil@gmail.com>
2017-01-24 13:34:14 +0100

    drop vote rewards on ground if player's inventory is full

--- src/main/java/tk/daporkchop/command/VoterewardCommand.java
+++ src/main/java/tk/daporkchop/command/VoterewardCommand.java
@@ -6,6 +6,7 @@ import java.util.Random;
 import cn.nukkit.Player;
 import cn.nukkit.Server;
 import cn.nukkit.command.CommandSender;
+import cn.nukkit.command.data.CommandParameter;
 import cn.nukkit.command.defaults.VanillaCommand;
 import cn.nukkit.item.Item;
 import cn.nukkit.utils.TextFormat;
@@ -13,7 +14,7 @@ import mobs.de.kniffo80.mobplugin.entities.utils.Utils;
 import tk.daporkchop.PorkUtils;
 
 public class VoterewardCommand extends VanillaCommand {
-	
+
 	public Random r = new Random();
 
 	public static final ArrayList<Item> item = new ArrayList<Item>();
@@ -36,34 +37,48 @@ public class VoterewardCommand extends VanillaCommand {
 		this.setPermission("nukkit.command.say"); // everyone should be able to
 													// use this
 		this.commandParameters.clear();
+		this.commandParameters.put("default",
+                new CommandParameter[]{
+                        new CommandParameter("player", CommandParameter.ARG_TYPE_TARGET, false)
+                });
 	}
 
 	@Override
 	public boolean execute(CommandSender sender, String commandLabel, String[] args) {
 		Player p = sender.getServer().getPlayer(args[0]);
 
-		if (p == null)	{
+		if (p == null) {
 			Server.getInstance().getLogger().alert("/votereward had no match, aborting");
 			return true;
 		}
-		
-		if (r.nextBoolean())	{
-			p.getInventory().addItem(item.get(r.nextInt(item.size())), item.get(r.nextInt(item.size())));
+		Item[][] leftOvers = new Item[2][0];
+		if (r.nextBoolean()) {
+			leftOvers[0] = p.getInventory().addItem(item.get(r.nextInt(item.size())), item.get(r.nextInt(item.size())));
 		} else {
-			if (r.nextBoolean())	{
-				p.getInventory().addItem(item.get(r.nextInt(item.size())), item.get(r.nextInt(item.size())), item.get(r.nextInt(item.size())));
+			if (r.nextBoolean()) {
+				leftOvers[0] = p.getInventory().addItem(item.get(r.nextInt(item.size())), item.get(r.nextInt(item.size())), item.get(r.nextInt(item.size())));
 			} else {
-				p.getInventory().addItem(item.get(r.nextInt(item.size())), item.get(r.nextInt(item.size())), item.get(r.nextInt(item.size())), item.get(r.nextInt(item.size())));
+				leftOvers[0] = p.getInventory().addItem(item.get(r.nextInt(item.size())), item.get(r.nextInt(item.size())), item.get(r.nextInt(item.size())), item.get(r.nextInt(item.size())));
 			}
 		}
-		if (Utils.rand(0, 50) == 27)	{
+		if (Utils.rand() && Utils.rand() && Utils.rand() && Utils.rand() && Utils.rand() && Utils.rand() && Utils.rand()) {
 			sender.getServer().broadcastMessage("" + TextFormat.BOLD + TextFormat.AQUA + p.getName() + TextFormat.GREEN + "WAS SUPER LUCKY AND GOT AN ELYTRA FROM VOTING!");
 			PorkUtils.queueMessageForDiscord("***" + p.getName() + " WAS SUPER LUCKY AND GOT AN ELYTRA FROM VOTING!***");
-			p.getInventory().addItem(Item.get(Item.ELYTRA, 0, 1));
+			leftOvers[1] = p.getInventory().addItem(Item.get(Item.ELYTRA, 0, 1));
+		}
+		if (leftOvers[0].length > 0) {
+			dropLeftOvers(p, leftOvers);
 		}
 		p.sendMessage("" + TextFormat.GOLD + TextFormat.BOLD + "Your vote items have been added!");
 
 		return true;
 	}
 
+	private static void dropLeftOvers(Player p, Item[]... toDrop) {
+		for (Item[] ia : toDrop) {
+			for (Item i : ia) {
+				p.getLevel().dropItem(p.temporalVector, i);
+			}
+		}
+	}
 }
