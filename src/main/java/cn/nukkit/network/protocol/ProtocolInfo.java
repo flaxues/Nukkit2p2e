9a319ad142b336185b1d5f6f81ea6bc721ad1d18
DaMatrix <joey.rabil@gmail.com>
2017-02-01 19:42:05 +0100

    :/

--- src/main/java/cn/nukkit/network/protocol/ProtocolInfo.java
+++ src/main/java/cn/nukkit/network/protocol/ProtocolInfo.java
@@ -9,7 +9,7 @@ public interface ProtocolInfo {
     /**
      * Actual Minecraft: PE protocol version
      */
-    byte CURRENT_PROTOCOL = 101;
+    byte CURRENT_PROTOCOL = 100;
     String MINECRAFT_VERSION = "v1.0.3.0";
     String MINECRAFT_VERSION_NETWORK = "1.0.3.0";
     
