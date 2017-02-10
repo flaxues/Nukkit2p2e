a154e3bc8da7de612abd59bef88c335d2b244b0d
DaMatrix <joey.rabil@gmail.com>
2017-02-10 09:49:58 +0100

    EnderCrystal test 1

--- src/main/java/cn/nukkit/item/ItemEndCrystal.java
+++ src/main/java/cn/nukkit/item/ItemEndCrystal.java
@@ -1,5 +1,18 @@
 package cn.nukkit.item;
 
+import java.util.Random;
+
+import cn.nukkit.Player;
+import cn.nukkit.block.Block;
+import cn.nukkit.entity.Entity;
+import cn.nukkit.entity.EntityEnderCrystal;
+import cn.nukkit.level.Level;
+import cn.nukkit.level.format.FullChunk;
+import cn.nukkit.nbt.tag.CompoundTag;
+import cn.nukkit.nbt.tag.DoubleTag;
+import cn.nukkit.nbt.tag.FloatTag;
+import cn.nukkit.nbt.tag.ListTag;
+
 public class ItemEndCrystal extends Item {
 
     public ItemEndCrystal() {
@@ -12,5 +25,50 @@ public class ItemEndCrystal extends Item {
 
     public ItemEndCrystal(Integer meta, int count) {
         super(END_CRYSTAL, meta, count, "End Crystal");
+    }
+    
+    @Override
+    public boolean canBeActivated() {
+        return true;
+    }
+    
+    @Override
+    public boolean onActivate(Level level, Player player, Block block, Block target, int face, double fx, double fy, double fz) {
+        FullChunk chunk = level.getChunk((int) block.getX() >> 4, (int) block.getZ() >> 4);
+
+        if (chunk == null) {
+            return false;
+        }
+
+        CompoundTag nbt = new CompoundTag()
+                .putList(new ListTag<DoubleTag>("Pos")
+                        .add(new DoubleTag("", block.getX() + 0.5))
+                        .add(new DoubleTag("", block.getY()))
+                        .add(new DoubleTag("", block.getZ() + 0.5)))
+                .putList(new ListTag<DoubleTag>("Motion")
+                        .add(new DoubleTag("", 0))
+                        .add(new DoubleTag("", 0))
+                        .add(new DoubleTag("", 0)))
+                .putList(new ListTag<FloatTag>("Rotation")
+                        .add(new FloatTag("", new Random().nextFloat() * 360))
+                        .add(new FloatTag("", 0)));
+
+        if (this.hasCustomName()) {
+            nbt.putString("CustomName", this.getCustomName());
+        }
+
+        Entity entity = Entity.createEntity(EntityEnderCrystal.NETWORK_ID, chunk, nbt);
+
+        if (entity != null) {
+            if (player.isSurvival()) {
+                Item item = player.getInventory().getItemInHand();
+                item.setCount(item.getCount() - 1);
+                player.getInventory().setItemInHand(item);
+            }
+            entity.spawnToAll();
+            return true;
+        }
+
+        return false;
     }
 }
