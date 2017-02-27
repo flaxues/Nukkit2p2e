2233f37e1189e52a1071446fde9d1daae25d3e51
DaMatrix <joey.rabil@gmail.com>
2017-02-27 21:25:00 +0100

    oops

--- src/main/java/mobs/milk/pureentities/task/AutoSpawnTask.java
+++ src/main/java/mobs/milk/pureentities/task/AutoSpawnTask.java
@@ -16,7 +16,7 @@ public class AutoSpawnTask implements Runnable{
     @SuppressWarnings("deprecation")
     public void run(){
         Server.getInstance().getOnlinePlayers().forEach((name, player) -> {
-            if(Utils.rand(1, 210) > 40){
+            if(Utils.rand()){
                 return;
             }
 
